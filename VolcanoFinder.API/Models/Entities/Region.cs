89a25aff252f8264b6ba3a5b9e57e4046f456aac
BenTec <bentec3d@gmail.com>
2022-06-22 17:37:56 +0200

    added Region and Volcano entities

---
+++ VolcanoFinder.API/Models/Entities/Region.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace VolcanoFinder.API.Models.Entities
+{
+    public class Region
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        public ICollection<Volcano> Volcanoes { get; set; } = new List<Volcano>();
+
+        public Region(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ VolcanoFinder.API/Models/Entities/Volcano.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace VolcanoFinder.API.Models.Entities
+{
+    public class Volcano
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Picture { get; set; }
+
+        [Required]
+        [MaxLength(2)]
+        public string CountryAlpha2 { get; set; }
+
+        [MaxLength(200)]
+        public string? Description { get; set; }
+
+        public DateTime? LastEruption { get; set; }
+
+        public bool? Active { get; set; }
+
+        [Required]
+        [ForeignKey("RegionId")]
+        public Region Region { get; set; }
+        public int RegionId { get; set; }
+
+
+        public Volcano(string name, string picture, int regionId)
+        {
+            Name = name;
+            Picture = picture;
+            RegionId = regionId;
+        }
+
+    }
+}
