dcdb13f1b00d71dda7df16bdad385a7f6a096451
BenTec <bentec3d@gmail.com>
2022-06-22 17:42:56 +0200

    added DbContext

---
+++ VolcanoFinder.API/DbContexts/VolcanoFinderContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VolcanoFinder.API.Models.Entities;
+
+namespace VolcanoFinder.API.DbContexts
+{
+    public class VolcanoFinderContext : DbContext
+    {
+        public DbSet<Region> Regions { get; set; } = null!;
+        public DbSet<Volcano> Volcanoes { get; set; } = null!;
+
+        public VolcanoFinderContext(DbContextOptions<VolcanoFinderContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- VolcanoFinder.API/Program.cs
+++ VolcanoFinder.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using VolcanoFinder.API.DbContexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Warning! Move ConnectionString to safe location!
+builder.Services.AddDbContext<VolcanoFinderContext>(dbContextOptions => dbContextOptions.UseSqlite(builder.Configuration["ConnectionStrings:VolcanoFinderDbConnectionString"]));
 
 var app = builder.Build();
 
