2c364bfd9e42d520069541558eed0f07f77426f3
BenTec <bentec3d@gmail.com>
2022-06-23 22:13:58 +0200

    including XML comments in swashbuckle

--- VolcanoFinder.API/Program.cs
+++ VolcanoFinder.API/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 using VolcanoFinder.API.DbContexts;
 using VolcanoFinder.API.Services;
 
@@ -14,7 +15,13 @@ builder.Services.AddControllers(options =>
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(setupAction =>
+{
+    var xmlCommentsFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentsFile);
+
+    setupAction.IncludeXmlComments(xmlCommentsFullPath);
+});
 
 //Warning! Move ConnectionString to safe location!
 builder.Services.AddDbContext<VolcanoFinderContext>(dbContextOptions => dbContextOptions.UseSqlite(builder.Configuration["ConnectionStrings:VolcanoFinderDbConnectionString"]));
