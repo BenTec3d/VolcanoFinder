43d2e0eec44c319bd5d07c91a9da63b5b354adff
BenTec <bentec3d@gmail.com>
2022-06-24 20:20:06 +0200

    added User

---
+++ VolcanoFinder.API/Models/DTOs/UserDto.cs
@@ -0,0 +1,18 @@
+﻿namespace VolcanoFinder.API.Models.DTOs
+{
+    /// <summary>
+    /// A user DTO for authentication
+    /// </summary>
+    public class UserDto
+    {
+        /// <summary>
+        /// The name of the user
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The password of the user
+        /// </summary>
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ VolcanoFinder.API/Models/Entities/User.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace VolcanoFinder.API.Models.Entities
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        public byte[] PasswordHash { get; set; }
+        [Required]
+        public byte[] PasswordSalt { get; set; }
+
+        public User(string name, byte[] passwordHash, byte[] passwordSalt)
+        {
+            Name = name;
+            PasswordHash = passwordHash;
+            PasswordSalt = passwordSalt;
+        }
+    }
+}
